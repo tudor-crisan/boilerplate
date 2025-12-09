13ebfc21d4e2f31a0a6dca3a937736bf32f61848
tudor-crisan <tudor.crisan.webdev@gmail.com>
2025-12-09 18:33:36 +0200

    created dahsboard page

---
+++ app/dashboard/page.js
@@ -0,0 +1,10 @@
+
+export default function Dashboard() {
+  return (
+    <main>
+      <h1>
+        Private dashboard
+      </h1>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -4,10 +4,10 @@ export default function Home() {
     <main>
       <h1>
         Collect customer feedback to build better products
-        <div>
-          Create a feedback board in minutes, prioritize features, and build produts your customers will love.
-        </div>
       </h1>
+      <div>
+        Create a feedback board in minutes, prioritize features, and build produts your customers will love.
+      </div>
     </main>
   );
 }
