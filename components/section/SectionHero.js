af3192c79a9b05aa9b7fe718db4a87df273835e1
tudor-crisan <tudor.crisan.webdev@gmail.com>
2025-12-10 21:46:09 +0200

    adjusted the SectionHero

--- components/section/SectionHero.js
+++ components/section/SectionHero.js
@@ -1,20 +1,35 @@
 import copywriting from "@/config/copywriting.json";
 import ButtonLogin from "@/components/button/ButtonLogin";
 import styling from "@/config/styling.json";
+import Image from "next/image";
+import demoImage from "@/public/images/demo.jpg";
 
 export default function SectionHero() {
   return (
-    <section className={`${styling.section.wrapper} ${styling.section.spacing} text-center space-y-6`}>
-      <h1 className={`${styling.SectionHero.headline}`}>
-        {copywriting.SectionHero.headline}
-      </h1>
-      <p className={`${styling.SectionHero.paragraph}`}>
-        {copywriting.SectionHero.paragraph}
-      </p>
-      <div>
-        <ButtonLogin
-          isLoggedIn={true}
-        />
+    <section className={`${styling.section.wrapper} ${styling.section.spacing}`}>
+      <div className="flex flex-col sm:flex-row sm:items-center space-y-6">
+        <div className="space-y-6">
+          <h1 className={`${styling.SectionHero.headline}`}>
+            {copywriting.SectionHero.headline}
+          </h1>
+          <p className={`${styling.SectionHero.paragraph}`}>
+            {copywriting.SectionHero.paragraph}
+          </p>
+          <div className={styling.SectionHero.button}>
+            <ButtonLogin
+              isLoggedIn={true}
+            />
+          </div>
+        </div>
+        {styling.SectionHero.hasDemoImage && (
+          <div className="max-w-sm mx-auto pl-0 sm:pl-6">
+            <Image
+              src={demoImage}
+              alt="Demo Image"
+              className="rounded-2xl shadow-md w-180"
+            />
+          </div>
+        )}
       </div>
     </section>
   );
