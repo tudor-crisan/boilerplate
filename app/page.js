8cefdb423184583c449271f113d5a470ca9c19e3
tudor-crisan <tudor.crisan.webdev@gmail.com>
2025-12-10 10:55:25 +0200

    added the hero section

--- app/page.js
+++ app/page.js
@@ -7,13 +7,15 @@ export default function Home() {
 
   return (
     <main>
-      <h1 style={{ fontSize: '48px', fontWeight: '800', lineHeight: '1' }}>
-        Collect customer feedback to build better products
-      </h1>
-      <div>
-        Create a feedback board in minutes, prioritize features, and build produts your customers will love.
-      </div>
-      <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      <section className="text-center py-32 px-8 max-w-3xl mx-auto">
+        <h1 className="text-4xl font-extrabold leading-none capitalize mb-6">
+          Collect customer feedback to build better products
+        </h1>
+        <p className="opacity-90 mb-10">
+          Create a feedback board in minutes, prioritize features, and build produts your customers will love.
+        </p>
+        <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      </section>
     </main>
   );
 }
