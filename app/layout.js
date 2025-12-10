50cb570ec86ea0bc6072b1fdf037d103b819250a
tudor-crisan <tudor.crisan.webdev@gmail.com>
2025-12-10 11:38:21 +0200

    added cupcake daisyui theme

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import "./globals.css";
+import styling from "@/config/styling.json";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme={styling.defaultTheme}>
       <body>
         {children}
       </body>
