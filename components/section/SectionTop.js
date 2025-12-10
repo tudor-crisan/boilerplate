873d42ee882888a5347fbc2972cb884ad08840c5
tudor-crisan <tudor.crisan.webdev@gmail.com>
2025-12-10 11:54:47 +0200

    modified the base colors

--- components/section/SectionTop.js
+++ components/section/SectionTop.js
@@ -1,10 +1,25 @@
+import copywriting from "@/config/copywriting.json";
+import menus from "@/config/menus.json";
+import ButtonLogin from "../button/ButtonLogin";
+import Link from "next/link";
 
 export default function SectionTop() {
+  const { appName } = copywriting.SectionTop;
+  const { topMenus } = menus;
+
   return (
-    <section>
-      <div></div>
-      <div></div>
-      <div></div>
+    <section className="bg-base-200">
+      <div>{appName}</div>
+      <div>
+        {topMenus.map((menu, index) => (
+          <Link href={menu.path} key={index}>
+            {menu.label}
+          </Link>
+        ))}
+      </div>
+      <div>
+        <ButtonLogin isLoggedIn={false} />
+      </div>
     </section>
   );
 }
