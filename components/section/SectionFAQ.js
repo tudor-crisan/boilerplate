0a9bb35cf4a7c86b3b0877eeaac89733a4713ead
tudor-crisan <tudor.crisan.webdev@gmail.com>
2025-12-10 19:20:11 +0200

    fixed the FAQ and other styling

--- components/section/SectionFAQ.js
+++ components/section/SectionFAQ.js
@@ -11,21 +11,13 @@ export default function SectionFAQ() {
         {copywriting.SectionFAQ.headline}
       </h2>
 
-      <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-        <input type="radio" name="my-accordion-2" defaultChecked />
-        <div className="collapse-title font-semibold">How do I create an account?</div>
-        <div className="collapse-content text-sm">Click the "Sign Up" button in the top right corner and follow the registration process.</div>
-      </div>
-      <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-        <input type="radio" name="my-accordion-2" />
-        <div className="collapse-title font-semibold">I forgot my password. What should I do?</div>
-        <div className="collapse-content text-sm">Click on "Forgot Password" on the login page and follow the instructions sent to your email.</div>
-      </div>
-      <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-        <input type="radio" name="my-accordion-2" />
-        <div className="collapse-title font-semibold">How do I update my profile information?</div>
-        <div className="collapse-content text-sm">Go to "My Account" settings and select "Edit Profile" to make changes.</div>
-      </div>
+      {copywriting.SectionFAQ.questions.map((faq, index) => (
+        <div className="collapse collapse-arrow bg-base-200 border border-base-300 my-2" key={index}>
+          <input type="radio" name="faq-accordion" defaultChecked={!index} />
+          <div className="collapse-title font-semibold text-primary">{faq.question}</div>
+          <div className="collapse-content text-sm">{faq.answer}</div>
+        </div>
+      ))}
     </section>
   );
 }
--- components/section/SectionTop.js
+++ components/section/SectionTop.js
@@ -1,5 +1,4 @@
 import copywriting from "@/config/copywriting.json";
-import menus from "@/config/menus.json";
 import ButtonLogin from "../button/ButtonLogin";
 import Link from "next/link";
 import Image from "next/image";
@@ -8,13 +7,13 @@ import styling from "@/config/styling.json";
 export default function SectionTop() {
   return (
     <section className="bg-base-200">
-      <div className={`${styling.section.wrapper} ${styling.SectionTop.positioning} px-4 py-2`}>
+      <div className={`${styling.section.wrapper} flex justify-between items-center px-4 py-2`}>
         <div className="flex items-center gap-1">
           <Image src="/icon.svg" alt={copywriting.SectionTop.appName} width="32" height="32" />
           <span className="font-bold">{copywriting.SectionTop.appName}</span>
         </div>
         <div className="space-x-4 max-md:hidden">
-          {menus.topMenus.map((menu, index) => (
+          {copywriting.SectionTop.menus.map((menu, index) => (
             <Link href={menu.path} key={index} className="link link-hover">
               {menu.label}
             </Link>
