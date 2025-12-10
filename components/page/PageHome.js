528f8c50b923c194cbc9a64f23d5e7dc33bf6d00
tudor-crisan <tudor.crisan.webdev@gmail.com>
2025-12-10 23:17:44 +0200

    added dynamic sections from styling

--- components/page/PageHome.js
+++ components/page/PageHome.js
@@ -1,16 +1,23 @@
-
+import styling from "@/config/styling.json";
 import SectionHero from "@/components/section/SectionHero";
 import SectionHeader from "@/components/section/SectionHeader";
 import SectionPricing from "@/components/section/SectionPricing";
 import SectionFAQ from "@/components/section/SectionFAQ";
 
+const COMPONENT_MAP = {
+  "SectionHeader": SectionHeader,
+  "SectionHero": SectionHero,
+  "SectionPricing": SectionPricing,
+  "SectionFAQ": SectionFAQ
+};
+
 export default function PageHome() {
   return (
     <main>
-      <SectionHeader />
-      <SectionHero />
-      <SectionPricing />
-      <SectionFAQ />
+      {styling.homepage.sections.map((key, index) => {
+        const Component = COMPONENT_MAP[key];
+        return Component ? <Component key={index} /> : null;
+      })}
     </main>
   );
 }
