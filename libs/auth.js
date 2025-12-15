15972610f858f7295e62eb4c8c4627b2f0c76e48
tudor-crisan <tudor.crisan.webdev@gmail.com>
2025-12-15 20:01:13 +0200

    set-up auth connections

--- libs/auth.js
+++ libs/auth.js
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/libs/mongo";
 
 const config = {
-  providers: []
+  providers: [],
+  adapter: MongoDBAdapter(clientPromise)
 };
 
 export const { handlers, signIn, signOut, auth } = NextAuth(config);
