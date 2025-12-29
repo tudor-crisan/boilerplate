abc8e9d1eca800548c9377425536275802456635
tudor-crisan <tudor.crisan.webdev@gmail.com>
2025-12-29 12:32:07 +0200

    refactored styling

--- libs/defaults.js
+++ libs/defaults.js
@@ -3,11 +3,11 @@ import stylings from "@/lists/stylings.js";
 import apps from "@/lists/apps.js";
 import visuals from "@/lists/visuals";
 import settings from "@/lists/settings";
-import { deepMerge } from "./merge";
+import { deepMerge } from "@/libs/utils.client";
 
 const { copywriting, styling, visual, setting } = apps[process.env.NEXT_PUBLIC_APP];
 
-export const defaultCopywriting = copywritings[copywriting];
+export const defaultCopywriting = deepMerge(copywritings.copywriting0, copywritings[copywriting]);
 export const defaultStyling = stylings[styling];
 export const defaultVisual = visuals[visual];
 export const defaultSetting = deepMerge(settings.setting0, settings[setting]);
