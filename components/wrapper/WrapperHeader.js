61f2e50efc70ab44b8cf5914476834b418221cd0
tudor-crisan <tudor.crisan.webdev@gmail.com>
2026-01-03 10:28:35 +0200

    update

--- components/wrapper/WrapperHeader.js
+++ components/wrapper/WrapperHeader.js
@@ -1,16 +1,10 @@
 "use client";
 import { useStyling } from "@/context/ContextStyling";
-import { useVisual } from "@/context/ContextVisual";
 
 export default function WrapperHeader({ children }) {
   const { styling } = useStyling();
-  const { visual } = useVisual();
-
-  const { menu, button } = visual.show.SectionHeader;
-  const flexClassName = styling.flex[(!menu && !button) ? 'center' : 'between'];
-
   return (
-    <div className={`${styling.general.container} ${flexClassName} ${styling.components.header}`}>
+    <div className={`${styling.general.container} ${styling.components.header} ${styling.flex.between}`}>
       {children}
     </div>
   )
