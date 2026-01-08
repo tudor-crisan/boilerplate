53414b145cc255dce12141894125f903350f4367
tudor-crisan <tudor.crisan.webdev@gmail.com>
2026-01-08 10:32:49 +0200

    updates

--- components/section/SectionApps.js
+++ components/section/SectionApps.js
@@ -7,34 +7,38 @@ import Paragraph from "@/components/common/Paragraph";
 import Title from "@/components/common/Title";
 import { cn } from "@/libs/utils.client";
 import Button from "@/components/button/Button";
-
-// Define the apps to display in this section
-const availableApps = ['loyalboards'];
+import { defaultSetting as settings } from "@/libs/defaults";
+import Flex from "@/components/common/Flex";
 
 const SectionAppsContent = () => {
-  availableApps.map((app) => {
+  const { styling } = useStyling();
+  return settings.availableApps.map((app) => {
     const { title, description, favicon, website, appName } = getAppDetails(app);
 
     return (
       <div key={app} className={`card w-full max-w-sm ${styling.components.card}`}>
         <div className={`card-body ${styling.general.box} space-y-6`}>
-          <div className={`flex items-center gap-2 font-extrabold`}>
-            {favicon && (
-              <Image
-                src={favicon}
-                alt={`${appName} logo`}
-                width={32}
-                height={32}
-                className={`${styling.components.element} object-contain`} // Safety for images
-              />
-            )}
-            <Title>{appName}</Title>
-          </div>
+          {(favicon || appName) && (
+            <div className={`flex items-center gap-2 font-extrabold`}>
+              {favicon && (
+                <Image
+                  src={favicon}
+                  alt={`${appName} logo`}
+                  className={`${styling.components.element} object-contain`} // Safety for images
+                  width={32}
+                  height={32}
+                />
+              )}
+              <Title>{appName}</Title>
+            </div>
+          )}
 
-          <div className="space-y-1">
-            <Title>{title}</Title>
-            <Paragraph>{description}</Paragraph>
-          </div>
+          {(title || description) && (
+            <div className="space-y-1">
+              {title && (<Title>{title}</Title>)}
+              {description && (<Paragraph>{description}</Paragraph>)}
+            </div>
+          )}
 
           {website && (
             <Button href={website}>
@@ -51,11 +55,11 @@ export default function SectionApps() {
   const { styling } = useStyling();
 
   return (
-    <section id="apps" className={cn(`${styling.general.container} ${styling.general.box} bg-base-100`, styling.SectionApps.padding)}>
-      {availableApps.length === 1 ? (
-        <div className={styling.flex.center}>
+    <section id="apps" className={cn(`${styling.general.container} ${styling.general.box} bg-base-100`, styling.SectionApps?.padding)}>
+      {settings.availableApps.length === 1 ? (
+        <Flex>
           <SectionAppsContent />
-        </div>
+        </Flex>
       ) : (
         <Grid>
           <SectionAppsContent />
