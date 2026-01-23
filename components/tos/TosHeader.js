2ce835734794c7a5cc7b0c79de9e6f2049e7fd0f
tudor-crisan <tudor.crisan.webdev@gmail.com>
2026-01-23 15:03:27 +0200

    updates

--- components/tos/TosHeader.js
+++ components/tos/TosHeader.js
@@ -1,7 +1,5 @@
 "use client";
-import ButtonBack from "@/components/button/ButtonBack";
-import HeaderTop from "@/components/header/HeaderTop";
-import WrapperHeader from "@/components/wrapper/WrapperHeader";
+import HeaderBack from "@/components/header/HeaderBack";
 import { defaultSetting as settings } from "@/libs/defaults";
 import { usePathname } from "next/navigation";
 
@@ -20,12 +18,5 @@ export default function TosHeader() {
     ? settings.paths.help.source
     : settings.paths.home.source;
 
-  return (
-    <section id="header" className="bg-base-200">
-      <WrapperHeader className="bg-base-200">
-        <HeaderTop url={settings.paths.home.source} />
-        <ButtonBack url={backUrl} />
-      </WrapperHeader>
-    </section>
-  );
+  return <HeaderBack backUrl={backUrl} />;
 }
